4f1e26bcf7861c0e49e4c53ab92146c704c506e4
Marco <m.rotondi@ngi.it>
2017-09-28 23:54:24 +0200

    Base version wiki example

--- vertx-wiki/src/main/java/com/marco/wiki/MainVerticle.java
+++ vertx-wiki/src/main/java/com/marco/wiki/MainVerticle.java
@@ -1,12 +1,34 @@
 package com.marco.wiki;
 
-import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.rxjava.core.AbstractVerticle;
+import org.flywaydb.core.Flyway;
+import rx.Single;
 
 public class MainVerticle extends AbstractVerticle {
 
     @Override
-    public void start() {
+    public void start(Future<Void> startFeature) throws Exception {
+        prepareDataBase()
+                .flatMap(v -> prepareWebServer())
+                .subscribe(startFeature::complete, startFeature::fail);
+    }
 
+
+    private Single<Void> prepareDataBase() {
+        return vertx.rxExecuteBlocking(future -> {
+            final Flyway flyway = new Flyway();
+            flyway.setDataSource(null, null, null);
+            flyway.migrate();
+            future.complete();
+        });
+    }
+
+    private Single<Void> prepareWebServer() {
+       return vertx.createHttpServer()
+                //.requestHandler(router::accept)
+                .rxListen(8080)
+                .map(server -> null);
     }
 
 }
